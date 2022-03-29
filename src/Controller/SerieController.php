fd1e020a68aabdf22aa65b1ce14347628fb1ca89
Herve BOISGONTIER <hboisgontier@eni-ecole.fr>
2022-03-29 14:50:32 +0200

    Manipulation (ajout, modification et suppression) d'une entité

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,5 +20,24 @@ class SerieController extends AbstractController
     public function details($id): Response {
         dd($id);
         return $this->render('serie/details.html.twig', ['id'=>$id]);
+    }
+
+    #[Route('/serie/add', name:'serie_add')]
+    public function add(EntityManagerInterface $em): Response {
+        // ajout
+        $serie = new Serie();
+        $serie->setName('Le bureau des légendes')
+            ->setTmdbId(125)
+            ->setDateCreated(new \DateTime());
+        $em->persist($serie);
+        $em->flush();
+        // modification
+        $serie->setName('Tutu')
+            ->setDateModified(new \DateTime());
+        $em->flush();
+        // suppression
+        $em->remove($serie);
+        $em->flush();
+        return $this->render('serie/add.html.twig');
     }
 }
